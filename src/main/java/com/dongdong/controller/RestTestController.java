c7e07af019fddc904809a60da5362d370c5d8187
zhaodexu <zhaodexu@vip.qq.com>
2018-09-11 16:14:27 +0800

    引入fastJson依赖

--- src/main/java/com/dongdong/controller/RestTestController.java
+++ src/main/java/com/dongdong/controller/RestTestController.java
@@ -1,9 +1,11 @@
 package com.dongdong.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.dongdong.consts.ResponseCode;
 import com.dongdong.consts.Result;
 import com.dongdong.entity.dao.UserTest;
 import com.dongdong.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class RestTestController {
 
@@ -19,11 +22,13 @@ public class RestTestController {
 
     @GetMapping("/rest")
     public Result<List<UserTest>> rest(){
+        log.info("获取人员全量信息");
         Result<List<UserTest>> result = new Result<List<UserTest>>();
         result.setCode(ResponseCode.SUCCESS.getCode());
         result.setMessage(ResponseCode.SUCCESS.getMessage());
         result.setData(userService.getAllUsers());
         result.setTimeStamp(Long.toString(System.currentTimeMillis()));
+        log.info("获取人员全量信息,结果:{}", JSONObject.toJSONString(result));
         return result;
     }
 }
