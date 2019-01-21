1d544926775f92f449be2c4979ae6c305cf68248
zhaodexu <zhaodexu@vip.qq.com>
2019-01-21 10:30:44 +0800

    update

--- src/main/java/com/dongdong/service/DepartmentService.java
+++ src/main/java/com/dongdong/service/DepartmentService.java
@@ -5,10 +5,26 @@ import com.dongdong.entity.vo.DepartmentVO;
 import com.dongdong.exception.BizException;
 
 public interface DepartmentService {
-
+    /**
+     * 创建Department
+     * @param departmentVO
+     * @throws BizException
+     */
     void createDepartment(DepartmentVO departmentVO) throws BizException;
 
+    /**
+     * 获取单个部门信息
+     * @param departmentVO
+     * @return
+     * @throws BizException
+     */
     DepartmentDTO getDepartmentDTO(DepartmentVO departmentVO) throws BizException;
 
+    /**
+     * 更新单个部门信息
+     * 使用乐观锁
+     * @param departmentVO
+     * @throws BizException
+     */
     void updateDepartment(DepartmentVO departmentVO) throws BizException;
 }
--- src/main/java/com/dongdong/service/PersonService.java
+++ src/main/java/com/dongdong/service/PersonService.java
@@ -5,10 +5,26 @@ import com.dongdong.entity.vo.PersonVO;
 import com.dongdong.exception.BizException;
 
 public interface PersonService {
-
+    /**
+     * 创建Person
+     * @param personVO
+     * @throws BizException
+     */
     void createPerson(PersonVO personVO) throws BizException;
 
+    /**
+     * 获取单个人员信息
+     * @param personVO
+     * @return
+     * @throws BizException
+     */
     PersonDTO getPersonDTO(PersonVO personVO) throws BizException;
 
+    /**
+     * 更新单个人员信息
+     * 使用乐观锁
+     * @param personVO
+     * @throws BizException
+     */
     void updatePerson(PersonVO personVO) throws BizException;
 }
